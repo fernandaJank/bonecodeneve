2008886efb3f38b2edcbba4ca0d1dbf909fbc087
fernandaJank <fernanda.jank@escola.pr.gov.br>
2024-09-23 15:31:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("purple");
+  stroke('white')
+  fill('white')
+  circle(200,100, 50 );
+  circle(200,165,100);
+  circle(200,255,150);
+  
+  fill('black')
+  circle(192,100,7);
+  circle(208,100,7);
+stroke('black')
+  fill('orange')
+   triangle(200, 105, 200, 110,230,115);
+  stroke('black')
+  line(195, 115, 205, 115)
+  
+  fill('brown')
+   circle(200,150, 10 );
+  circle(200,170,10);
+  circle(200,190,10);
+  
+  fill('black')
+  stroke('black')
+  line(160,155,100,165);
+  line(240,155,300,165);
+}
